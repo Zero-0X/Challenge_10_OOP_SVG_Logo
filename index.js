79f6a8509de09752c1626578680d960318db1570
Zero_X <rvkov536@gmail.com>
2023-07-09 00:28:20 -0400

    installed packages, and updated index js ile, going to work on the shapes file next

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-// GIVEN a command-line application that accepts user input
+const inquirer = require('inquirer');
+const { createLogo } = require('./lib/createLogo');
 
-// WHEN I am prompted for text
-// THEN I can enter up to three characters
+const runApplication = async () => {
+  const answers = await inquirer.prompt([
+    {
+      name: 'text',
+      message: 'Enter up to three characters:',
+      validate: (input) => input.length <= 3 || 'Please enter up to three characters.',
+    },
+    {
+      name: 'textColor',
+      message: 'Enter the text color (color keyword or hexadecimal number):',
+    },
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Choose a shape:',
+      choices: ['circle', 'triangle', 'square'],
+    },
+    {
+      name: 'shapeColor',
+      message: 'Enter the shape color (color keyword or hexadecimal number):',
+    },
+  ]);
 
-// WHEN I am prompted for the text color
-// THEN I can enter a color keyword (OR a hexadecimal number)
+  await createLogo(answers);
+};
 
-// WHEN I am prompted for a shape
-// THEN I am presented with a list of shapes to choose from: circle, triangle, and square
-
-// WHEN I am prompted for the shape's color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-
-// WHEN I have entered input for all the prompts
-// THEN an SVG file is created named `logo.svg`
-// AND the output text "Generated logo.svg" is printed in the command line
-
-// WHEN I open the `logo.svg` file in a browser
-// THEN I am shown a 300x200 pixel image that matches the criteria I entered
+runApplication();
